88b9417dd0bf1825f42e46d4c0d457d5609f83b7
Khochawongwat <khochawongwat.kongpana@gmail.com>
2023-12-12 19:21:39 +0800

    Pomodoro Redesigned

--- frontend/src/components/features/Pomodoro/PomoBoard.tsx
+++ frontend/src/components/features/Pomodoro/PomoBoard.tsx
@@ -1,33 +1,7 @@
 import { useEffect, useState } from "react";
-import { Grid, IconButton, Button, Typography } from "@mui/material";
-import { ArrowBackIosNewOutlined, ArrowForwardIosOutlined } from "@mui/icons-material";
+import { Grid, Button, Box, Typography } from "@mui/material";
 import { myPalette } from "../../../theme";
-import PomoCard from "./PomoCard";
 import Pomo from "../../../schema/Pomo.schema";
-
-const PaperStyle = {
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  width: "12rem",
-  mx: '.5rem',
-  aspectRatio: 1 / 0.9,
-  background: myPalette[400],
-  borderRadius: "8px",
-  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-};
-
-const PreviousPaperStyle = {
-  opacity: 0.4,
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  width: "8rem",
-  aspectRatio: 1 / 0.9,
-  background: myPalette[400],
-  borderRadius: "8px",
-  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-};
 
 const PomoBoard = () => {
   const [selectedTask, setSelectedTask] = useState(0);
@@ -36,52 +10,40 @@ const PomoBoard = () => {
   const [remainingTime, setRemainingTime] = useState<number>(0)
 
   useEffect(() => {
-    // Create a Pomo instance with a session duration of 25 minutes
-    const pomo = new Pomo(25);
+    const savedPomo = localStorage.getItem("pomodoro")
+    if (savedPomo && savedPomo.length > 0) {
+      const pomo = new Pomo(JSON.parse(savedPomo))
+      pomo.setCallbacks({
+        onTick: (time: number) => setRemainingTime(time),
+        onStart: () => console.log('Pomo session started!'),
+        onPause: () => console.log('Pomo session paused!'),
+        onReset: () => console.log('Pomo session reset!'),
+        onComplete: () => console.log('Pomo session completed!'),
+      });
 
-    // Set callbacks to update UI
-    pomo.setCallbacks({
-      onTick: (time) => setRemainingTime(time),
-      onStart: () => console.log('Pomo session started!'),
-      onPause: () => console.log('Pomo session paused!'),
-      onReset: () => console.log('Pomo session reset!'),
-      onComplete: () => console.log('Pomo session completed!'),
-    });
+      setPomoSession(pomo);
 
-    setPomoSession(pomo);
-
-    // Cleanup when the component unmounts
-    return () => {
-      if (pomoSession) pomoSession.reset();
-    };
-  }, []); // Run this effect only once on mount
-  useEffect(() => {
-    const session = localStorage.getItem("pomodoro")
-    if (session && session.length > 0) {
-      setPomoSession(JSON.parse(session))
+      return () => {
+        if (pomoSession) {
+          pomoSession.reset()
+          pomoSession.start()
+        };
+      };
     }
-  }, [])
-
-  const handleNext = () => {
-    setSelectedTask((prevTask) => (prevTask + 1) % tasks.length);
-  };
-
-  const handlePrev = () => {
-    setSelectedTask((prevTask) => (prevTask - 1 + tasks.length) % tasks.length);
-  };
-
-  const getPreviousTaskIndex = (currentIndex: any) => (currentIndex - 1 + tasks.length) % tasks.length;
-  const getNextTaskIndex = (currentIndex: any) => (currentIndex + 1) % tasks.length;
+  }, []);
 
   const startPomoSession = () => {
     const newPomoSession = new Pomo();
     setPomoSession(newPomoSession);
     localStorage.setItem("pomodoro", JSON.stringify(newPomoSession));
   };
+
   const handleStop = () => {
-    setPomoSession(null);
     localStorage.removeItem("pomodoro");
+    setPomoSession(null);
+    console.log("Removed");
   };
+
 
   return (
     <Grid
@@ -94,31 +56,24 @@ const PomoBoard = () => {
       container
       spacing={2}
     >
-      <Grid item xs={12}>
-        <Grid container justifyContent="center" p="32px" alignItems="center">
-          {pomoSession ? (
-            <>
-              <Grid item>
-                <IconButton onClick={handlePrev}>
-                  <ArrowBackIosNewOutlined />
-                </IconButton>
-              </Grid>
-              <PomoCard task={tasks[getPreviousTaskIndex(selectedTask)]} style={PreviousPaperStyle} />
-              <PomoCard task={tasks[selectedTask]} style={PaperStyle} />
-              <PomoCard task={tasks[getNextTaskIndex(selectedTask)]} style={PreviousPaperStyle} />
-              <Button onClick={handleStop}>Stop</Button>
-              <Grid item>
-                <IconButton onClick={handleNext}>
-                  <ArrowForwardIosOutlined />
-                </IconButton>
-              </Grid>
-            </>
-          ) : (
-            <>
-              <Button onClick={startPomoSession}>Start a session!</Button>
-            </>
-          )}
-        </Grid>
+      <Grid item xs={12} display="flex" justifyContent="center" flexDirection="column" alignItems="center">
+        <Box display="flex" justifyContent="center" flexDirection="column" px="32px" py="16px">
+          {pomoSession ? <Box sx={{ display: 'flex', justifyContent: 'space-around' }}>
+            <Button>Pomodoro</Button>
+            <Button>Short Break</Button>
+            <Button>Long Break</Button>
+          </Box> : null}
+          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            {pomoSession ? (
+              <>
+                <Typography sx={{ fontSize: '6rem' }}>
+                  25:00
+                </Typography>
+              </>
+            ) : null}
+            <Button onClick={startPomoSession}>Start</Button>
+          </Box>
+        </Box>
       </Grid>
     </Grid>
   );
